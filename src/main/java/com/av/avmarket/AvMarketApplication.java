1a3c1182fcada30415c1598df1195d6843976ed9
alonso <alonsovillegas2016@gmail.com>
2020-12-31 02:58:57 -0500

    Initial code

---
+++ src/main/java/com/av/avmarket/AvMarketApplication.java
@@ -0,0 +1,13 @@
+package com.av.avmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/av/avmarket/AvMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.av.avmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
