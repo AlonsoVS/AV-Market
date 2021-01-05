f3bf27ab0d74771bd1f3a35d2459cee4b64227f4
alonso <alonsovillegas2016@gmail.com>
2021-01-04 22:26:24 -0500

    Expose the API
    Add GetMapping to controller methods to expose API functions

--- src/main/java/com/av/avmarket/web/controller/ProductController.java
+++ src/main/java/com/av/avmarket/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.av.avmarket.web.controller;
 import com.av.avmarket.domain.ProductDomain;
 import com.av.avmarket.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,13 +15,18 @@ public class ProductController {
   @Autowired
   private ProductService productService;
 
+  @GetMapping("/all")
   public List<ProductDomain> getAll() {return productService.getAll();}
 
-  public Optional<ProductDomain> getProduct(int productId) {return productService.getProduct(productId);}
+  @GetMapping("/{id}")
+  public Optional<ProductDomain> getProduct(@PathVariable("id") int productId) {return productService.getProduct(productId);}
 
-  public Optional<List<ProductDomain>> getByCategory(int categoryId) {return productService.getByCategory(categoryId);}
+  @GetMapping("/category/{categoryId}")
+  public Optional<List<ProductDomain>> getByCategory(@PathVariable("categoryId") int categoryId) {return productService.getByCategory(categoryId);}
 
-  public ProductDomain save(ProductDomain productDomain) {return productService.save(productDomain);}
+  @PostMapping("/save")
+  public ProductDomain save(@RequestBody ProductDomain productDomain) {return productService.save(productDomain);}
 
-  public boolean delete(int productId) {return productService.delete(productId);}
+  @DeleteMapping("/delete/{id}")
+  public boolean delete(@PathVariable("id") int productId) {return productService.delete(productId);}
 }
