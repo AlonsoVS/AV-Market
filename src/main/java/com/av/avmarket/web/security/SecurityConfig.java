926d8fa8dccccd22be8ca12b3f8208840a1cafa3
alonso <alonsovillegas2016@gmail.com>
2021-01-06 15:23:23 -0500

    Add JWT-based authorization
    Add authorization for requests using the generated JSON web token

--- src/main/java/com/av/avmarket/web/security/SecurityConfig.java
+++ src/main/java/com/av/avmarket/web/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.av.avmarket.web.security;
 
 import com.av.avmarket.domain.service.AvUserDetailsService;
+import com.av.avmarket.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,13 +10,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
   private AvUserDetailsService avUserDetailsService;
+
+  @Autowired
+  private JwtFilterRequest jwtFilterRequest;
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -34,7 +40,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
-          .anyRequest().authenticated();
+          .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
   }
 
   @Override
